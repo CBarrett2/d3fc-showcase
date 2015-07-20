44ac544a0d218478bcf3c72ace629290da57810c
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-07-20 12:06:41 +0100

    having a git nightmare, sorry for these commits

--- src/assets/js/zoomPlugin.js
+++ src/assets/js/zoomPlugin.js
@@ -5,6 +5,22 @@
         function zoom(selection, chart) {
             zoomBehavior.x(chart.dateScale())
                 .on('zoom', function() {
+                    // Set furthest left and right points you can pan
+                    var dataExtent = fc.util.extent(selection.data(), ['date']);
+                    var tx = zoomBehavior.translate()[0];
+                    var ty = zoomBehavior.translate()[1];
+
+                    var min = chart.dateScale()(dataExtent[0]);
+                    var max = chart.dateScale()(dataExtent[1]);
+
+                    if (min >= 0) {
+                        tx = Math.max(0, tx - min);
+                    }
+                    if ((max - chart.width()) <= 0) {
+                        tx = Math.min(0, tx - (max - chart.width()));
+                    }
+                    zoomBehavior.translate([tx, ty]);
+
                     // Recalling chart draws the new view, this is a place-holder for drawing/two way binding
                     chart(selection);
                 });
