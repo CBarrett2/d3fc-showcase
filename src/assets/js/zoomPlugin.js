c975580c70b2627057963fbb40820c64ebd70ac9
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-07-20 16:52:20 +0100

    Fixed zoom so it zooms to mouse cursor

--- src/assets/js/zoomPlugin.js
+++ src/assets/js/zoomPlugin.js
@@ -2,24 +2,26 @@
     'use strict';
     fcsc.zoomPlugin = function() {
         var zoomBehavior = d3.behavior.zoom();
-        var lastX;
+        var lastX, lastY;
         function zoom(selection, chart) {
+            var width = chart.xScale().range()[0];
+            var height = chart.xScale().range()[1];
             zoomBehavior.x(chart.xScale())
                 .on('zoomstart', function() {
                     lastX = d3.mouse(this)[0];
                 })
                 .on('zoom', function() {
-                    var tx = d3.mouse(this)[0] - lastX;
-                    var ty = zoomBehavior.translate()[1];
-
+                    
+                    if(zoomBehavior.scale() == 1){
+                        var tx = d3.mouse(this)[0] - lastX;
+                        var ty = zoomBehavior.translate()[1];                    
+                        zoomBehavior.translate([tx, ty]);
+                    }
                     lastX = d3.mouse(this)[0];
-
-                    zoomBehavior.translate([tx, ty]);
-
                     // Recalling chart draws the new view, this is a place-holder for drawing/two way binding
-                    //selection.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
                     chart(selection);
                 });
+            
             selection.call(zoomBehavior);
             return zoom;
         }
