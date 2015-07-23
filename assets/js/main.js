3b0626a1e1db63daf8eb1fef135b62bb0adbc16b
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-07-23 09:09:44 +0100

    Deploy to GitHub Pages

---
+++ assets/js/main.js
@@ -0,0 +1,202 @@
+(function(d3, fc) {
+    'use strict';
+    function getVisibleData(data, dateExtent) {
+        // Calculate visible data, given [startDate, endDate]
+        var bisector = d3.bisector(function(d) { return d.date; });
+        var visibleData = data.slice(
+        // Pad and clamp the bisector values to ensure extents can be calculated
+            Math.max(0, bisector.left(data, dateExtent[0]) - 1),
+            Math.min(bisector.right(data, dateExtent[1]) + 1, data.length)
+        );
+        return visibleData;
+    }
+
+    var width = 500;
+    var height = 300;
+    var rsiHeight = height / 2;
+    var navHeight = height / 3;
+
+
+    // Set SVGs
+    var container = d3.select('#chart-example');
+    var svgMain = container.select('svg.main')
+        .attr('viewBox', function() { return '0 0 ' + width + ' ' + height; })
+        .attr('width', width)
+        .attr('height', height);
+
+    var svgRSI = container.select('svg.rsi')
+        .attr('viewBox', function() { return '0 0 ' + width + ' ' + rsiHeight; })
+        .attr('width', width)
+        .attr('height', rsiHeight);
+
+    var svgNav = container.select('svg.nav')
+        .attr('viewBox', function() { return '0 0 ' + width + ' ' + navHeight; })
+        .attr('width', width)
+        .attr('height', navHeight);
+
+    var data = fc.data.random.financial()(250);
+
+    // Create main chart and set how much data is initially viewed
+    var timeSeries = fc.chart.linearTimeSeries()
+        .xDomain([data[Math.floor(data.length / 2)].date, data[Math.floor(data.length * 3 / 4)].date])
+        .xTicks(6);
+
+    var candlestick = fc.series.candlestick();
+    var gridlines = fc.annotation.gridline()
+        .yTicks(5)
+        .xTicks(0);
+
+    // Create and apply the Moving Average
+    var movingAverage = fc.indicator.algorithm.movingAverage();
+
+    // Create a line that renders the result
+    var ma = fc.series.line()
+        .yValue(function(d) { return d.movingAverage; });
+
+    var multi = fc.series.multi().series([candlestick, gridlines, ma]);
+
+    function zoomCall(zoom, data, scale) {
+        return function() {
+            var tx = zoom.translate()[0];
+            var ty = zoom.translate()[1];
+
+            var xExtent = fc.util.extent(data, ['date']);
+            var min = scale(xExtent[0]);
+            var max = scale(xExtent[1]);
+
+            // Don't pan off sides
+            if (min > 0) {
+                tx -= min;
+            } else if (max - width < 0) { // or use xScale.range()
+                tx -= (max - width);
+            }
+
+            // If zooming, and about to pan off screen, do nothing
+            if (zoom.scale() !== 1) {
+                if ((min > 0) && (max - width) < 0) {
+                    tx = 0;
+                    scale.domain(xExtent);
+                    zoom.scale(1);
+                }
+            }
+
+            zoom.translate([tx, ty]);
+            render();
+        };
+    }
+
+    var mainChart = function(selection) {
+        data = selection.datum();
+        movingAverage(data);
+
+
+        // Scale y axis
+        var yExtent = fc.util.extent(getVisibleData(data, timeSeries.xDomain()), ['low', 'high']);
+        // Add 10% either side of extreme high/lows
+        var variance = yExtent[1] - yExtent[0];
+        yExtent[0] -= variance * 0.1;
+        yExtent[1] += variance * 0.1;
+        timeSeries.yDomain(yExtent);
+
+        // Redraw
+        timeSeries.plotArea(multi);
+        selection.call(timeSeries);
+
+        // Behaves oddly if not reinitialized every render
+        var zoom = d3.behavior.zoom();
+        zoom.x(timeSeries.xScale())
+            .on('zoom', zoomCall(zoom, data, timeSeries.xScale()));
+
+        selection.call(zoom);
+    };
+
+    // Create RSI chart
+    var rsiScale = d3.scale.linear()
+        .domain([0, 100])
+        .range([rsiHeight, 0]);
+    var rsiAlgorithm = fc.indicator.algorithm.relativeStrengthIndex();
+
+    var rsi = fc.indicator.renderer.relativeStrengthIndex()
+        .xScale(timeSeries.xScale())
+        .yScale(rsiScale);
+
+    var rsiChart = function(selection) {
+        data = selection.datum();
+        rsiAlgorithm(data);
+
+        // Important for initialization that this happens after timeSeries is called [or can call render() twice]
+        var zoom = d3.behavior.zoom();
+        zoom.x(timeSeries.xScale())
+            .on('zoom', zoomCall(zoom, data, timeSeries.xScale()));
+        selection.call(zoom);
+        selection.call(rsi);
+    };
+
+    // Create navigation chart
+    var yExtent = fc.util.extent(getVisibleData(data, fc.util.extent(data, 'date')), ['low', 'high']);
+    var navTimeSeries = fc.chart.linearTimeSeries()
+        .xDomain(fc.util.extent(data, 'date'))
+        .yDomain(yExtent)
+        .yTicks(5);
+
+    var area = fc.series.area()
+        .yValue(function(d) { return d.open; })
+        .y0Value(yExtent[0]);
+
+    var line = fc.series.line()
+        .yValue(function(d) { return d.open; });
+
+    var brush = d3.svg.brush();
+    var navMulti = fc.series.multi().series([area, line, brush]);
+
+    var navChart = function(selection) {
+        data = selection.datum();
+
+        brush.on('brush', function() {
+                if (brush.extent()[0][0] - brush.extent()[1][0] !== 0) {
+                    // Control the main chart's time series domain
+                    timeSeries.xDomain([brush.extent()[0][0], brush.extent()[1][0]]);
+                    render();
+                }
+            });
+
+        // Allow to zoom using mouse, but disable panning
+        var zoom = d3.behavior.zoom()
+            .x(timeSeries.xScale())
+            .on('zoom', function() {
+                if (zoom.scale() === 1) {
+                    zoom.translate([0, 0]);
+                } else {
+                    render();
+                }
+            });
+        selection.call(zoom);
+
+        navMulti.mapping(function(series) {
+                if (series === brush) {
+                    brush.extent([
+                        [timeSeries.xDomain()[0], navTimeSeries.yDomain()[0]],
+                        [timeSeries.xDomain()[1], navTimeSeries.yDomain()[1]]
+                    ]);
+                }
+                return data;
+            });
+
+        navTimeSeries.plotArea(navMulti);
+        selection.call(navTimeSeries);
+    };
+
+    function render() {
+        svgMain.datum(data)
+            .call(mainChart);
+
+        svgRSI.datum(data)
+            .call(rsiChart);
+
+        svgNav.datum(data)
+            .call(navChart);
+    }
+
+    render();
+
+})(d3, fc);
