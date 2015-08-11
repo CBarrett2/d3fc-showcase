bdff8b5999674ff1f62cd82773dde5abc741c3a5
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-11 16:57:15 +0100

    Fixed single data point display

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,16 @@
 (function(d3, fc, sc) {
     'use strict';
 
+    function padExtent(extent) {
+        // Adds a buffer of current period to either side of data
+        var period = ohlc.period();
+
+        extent[0] = d3.time.second.offset(new Date(extent[0]), -period);
+        extent[1] = d3.time.second.offset(new Date(extent[1]), +period);
+
+        return extent;
+    }
+
     // Set SVGs & column padding
     var container = d3.select('#chart-example');
 
@@ -30,12 +40,8 @@
             .attr('height', mainHeightRatio * targetHeight / totalHeightRatio);
         svgRSI.attr('width', targetWidth)
             .attr('height', rsiHeightRatio * targetHeight / totalHeightRatio);
-        svgNav.attr('width', targetWidth);
-
-        var navAspect = navHeight / targetWidth;
-
-        standardDateDisplay = [data[Math.floor((1 - navAspect * goldenRatio) * data.length)].date,
-            data[data.length - 1].date];
+        svgNav.attr('width', targetWidth)
+            .attr('height', navHeightRatio * targetHeight / totalHeightRatio);
     }
 
     var candlestick = fc.series.candlestick();
@@ -90,58 +96,111 @@
             changeSeries(seriesTypeString);
         });
 
-    // Set Reset button event
+    var ohlc = sc.data.feed.coinbase.ohlcWebSocketAdaptor();
+    // Default to generated data
+    var currData = fc.data.random.financial()(250);
+
+    function liveCallback(event, latestBasket) {
+        if (!event && latestBasket) {
+            if (!currData.length) {
+                currData = [latestBasket];
+                resetToLive();
+                hideOrShowChartsAndSetLoadingText(false);
+            } else if (currData[currData.length - 1].date.getTime() !== latestBasket.date.getTime()) {
+                currData.push(latestBasket);
+            } else {
+                currData[currData.length - 1] = latestBasket;
+            }
+            console.log(currData);
+            render();
+        } else if (event.type === 'open') {
+            // On successful open
+            hideOrShowChartsAndSetLoadingText(true, 'Connected, waiting for data...');
+        } else if (event.type === 'close' && event.code === 1000) {
+            // I don't think there's any need for a message on successful close
+        } else {
+            hideOrShowChartsAndSetLoadingText(true, 'Error loading data from coinbase websocket: ' +
+                event.type + ' ' + event.code);
+        }
+    }
+
     function resetToLive() {
+        // Using golden ratio to make initial display area rectangle into the golden rectangle
+        if (!currData.length) {
+            return;
+        }
+        var goldenRatio = 1.618;
+        var navAspect = parseInt(svgNav.attr('height'), 10) / parseInt(svgNav.attr('width'), 10);
+        var standardDateDisplay = [currData[Math.floor((1 - navAspect * goldenRatio) * currData.length)].date,
+                currData[currData.length - 1].date];
+        standardDateDisplay = padExtent(standardDateDisplay);
         timeSeries.xDomain(standardDateDisplay);
         render();
     }
 
-    container.select('#reset-button').on('click', resetToLive);
+    d3.select('#type-selection')
+        .on('change', function() {
+            var type = d3.select(this).property('value');
+            if (type === 'live') {
+                d3.select('#period-span').style('visibility', 'visible');
+                d3.select('#product-span').style('visibility', 'visible');
+                currData = [];
+                hideOrShowChartsAndSetLoadingText(true, 'Connecting to websocket...');
+                render();
+                ohlc(liveCallback);
+            } else if (type === 'fake') {
+                // maybe generate from scratch
+                d3.select('#period-span').style('visibility', 'hidden');
+                d3.select('#product-span').style('visibility', 'hidden');
+                ohlc.close();
+                hideOrShowChartsAndSetLoadingText(false);
+                currData = fc.data.random.financial()(250);
+                // No need for loading text as this will be instant
+                resetToLive();
+                render();
+            }
+        });
+
+    d3.select('#period-selection')
+        .on('change', function() {
+            var period = parseInt(d3.select(this).property('value'));
+            ohlc.period(period);
+            currData = [];
+            hideOrShowChartsAndSetLoadingText(true, 'Connecting to websocket...');
+            render();
+            ohlc(liveCallback);
+        });
+
+    d3.select('#product-selection')
+        .on('change', function() {
+            var product = d3.select(this).property('value');
+            // Would be nice to base this selection off the products list
+            ohlc.product(product);
+            currData = [];
+            hideOrShowChartsAndSetLoadingText(true, 'Connecting to websocket...');
+            render();
+            ohlc(liveCallback);
+        });
+
+
 
     // Create main chart and set how much data is initially viewed
     var timeSeries = fc.chart.linearTimeSeries()
-        .xDomain(standardDateDisplay)
         .xTicks(6);
 
     var gridlines = fc.annotation.gridline()
         .yTicks(5)
         .xTicks(0);
 
-    function calculateCloseAxisTagPath(width, height) {
-        var h2 = height / 2;
-        return [
-            [0, 0],
-            [h2, -h2],
-            [width, -h2],
-            [width, h2],
-            [h2, h2],
-            [0, 0]
-        ];
-    }
+    var startPriceLine = fc.annotation.line()
+        .orient('horizontal')
+        .value(function(d) { return d.open; })
+        .label(function(d) { return 'OPEN'; });
 
-    function positionCloseAxis(sel) {
-        sel.enter()
-            .select('.right-handle')
-            .insert('path', ':first-child')
-            .attr('transform', 'translate(' + -40 + ', 0)')
-            .attr('d', d3.svg.area()(calculateCloseAxisTagPath(40, 14)));
-
-        sel.select('text')
-            .attr('transform', 'translate(' + (-2) + ', ' + 2 + ')')
-            .attr('x', 0)
-            .attr('y', 0);
-    }
-
-    var priceFormat = d3.format('.2f');
-
-    var closeAxisAnnotation = fc.annotation.line()
+    var endPriceLine = fc.annotation.line()
         .orient('horizontal')
         .value(function(d) { return d.close; })
-        .label(function(d) { return priceFormat(d.close); })
-        .decorate(function(sel) {
-            positionCloseAxis(sel);
-            sel.enter().classed('close', true);
-        });
+        .label(function(d) { return 'CLOSE'; });
 
     // Create and apply the Moving Average
     var movingAverage = fc.indicator.algorithm.movingAverage();
@@ -189,7 +248,7 @@
             var tx = zoom.translate()[0];
             var ty = zoom.translate()[1];
 
-            var xExtent = fc.util.extent(data, ['date']);
+            var xExtent = padExtent(fc.util.extent(data, ['date']));
             var min = scale(xExtent[0]);
             var max = scale(xExtent[1]);
 
@@ -215,13 +274,29 @@
     }
 
     var mainChart = function(selection) {
-        data = selection.datum();
+        var data = selection.datum();
         movingAverage(data);
+
+        multi.mapping(function(series) {
+            switch (series) {
+                case startPriceLine:
+                    return [data[0]];
+                case endPriceLine:
+                    return [data[data.length - 1]];
+                default:
+                    return data;
+            }
+        });
 
         // Scale y axis
         var yExtent = fc.util.extent(sc.util.filterDataInDateRange(data, timeSeries.xDomain()), ['low', 'high']);
         // Add 10% either side of extreme high/lows
         var variance = yExtent[1] - yExtent[0];
+        if (variance === 0) {
+            // Bit of a hack to get single-value candlesticks to show nicely.
+            // Shouldn't be needed when historic data integrated
+            variance = 1;
+        }
         yExtent[0] -= variance * 0.1;
         yExtent[1] += variance * 0.1;
         timeSeries.yDomain(yExtent);
@@ -248,7 +323,7 @@
         .yScale(rsiScale);
 
     var rsiChart = function(selection) {
-        data = selection.datum();
+        var data = selection.datum();
         rsi.xScale(timeSeries.xScale());
         rsi.yScale().range([parseInt(svgRSI.style('height'), 10), 0]);
         rsiAlgorithm(data);
@@ -261,14 +336,12 @@
     };
 
     // Create navigation chart
-    var yExtent = fc.util.extent(sc.util.filterDataInDateRange(data, fc.util.extent(data, 'date')), ['low', 'high']);
     var navTimeSeries = fc.chart.linearTimeSeries()
         .xDomain(fc.util.extent(data, 'date'))
-        .yDomain(yExtent)
         .yTicks(0);
 
-    area.yValue(function(d) { return d.open; })
-        .y0Value(yExtent[0]);
+    var area = fc.series.area()
+        .yValue(function(d) { return d.open; });
 
     line.yValue(function(d) { return d.open; });
 
@@ -276,7 +349,13 @@
     var navMulti = fc.series.multi().series([area, line, brush]);
 
     var navChart = function(selection) {
-        data = selection.datum();
+        var data = selection.datum();
+
+        var xExtent = padExtent(fc.util.extent(data, 'date'));
+        var yExtent = fc.util.extent(getVisibleData(data, fc.util.extent(data, 'date')), ['low', 'high']);
+        navTimeSeries.xDomain(xExtent)
+            .yDomain(yExtent);
+        area.y0Value(yExtent[0]);
 
         brush.on('brush', function() {
                 if (brush.extent()[0][0] - brush.extent()[1][0] !== 0) {
@@ -313,13 +392,68 @@
         selection.call(navTimeSeries);
     };
 
+    function hideOrShowChartsAndSetLoadingText(hidden, text) {
+        function setLoadingText(text) {
+            if (text) {
+                d3.select('#loading-text')
+                    .text(text);
+            }
+        }
+
+        function toggleVisibility(hidden) {
+            var visibility = (hidden === false) ? 'visible' : 'hidden';
+            var textVisibility = (hidden === false) ? 'hidden' : 'visible';
+
+            var type = d3.select('#type-selection').property('value');
+
+            if (type === 'live') {
+                svgRSI.style('visibility', 'hidden');
+                svgNav.style('visibility', 'hidden');
+            } else {
+                svgRSI.style('visibility', visibility);
+                svgNav.style('visibility', visibility);
+            }
+
+            svgMain.style('visibility', visibility);
+            d3.select('#loading-text')
+                .style('visibility', textVisibility);
+
+        }
+
+        toggleVisibility(hidden);
+        setLoadingText(text);
+    }
+
+    function render() {
+        if (!currData.length) {
+            return;
+        }
+
+        svgMain.datum(currData)
+            .call(mainChart);
+
+        svgRSI.datum(currData)
+            .call(rsiChart);
+
+        svgNav.datum(currData)
+            .call(navChart);
+    }
+
     function resize() {
         calculateDimensions();
         render();
     }
 
-    d3.select(window).on('resize', resize);
+    d3.select('#period-span').style('visibility', 'hidden');
+    d3.select('#product-span').style('visibility', 'hidden');
+    d3.select('#loading-text')
+        .style('visibility', 'hidden');
 
+    calculateDimensions();
+
+    container.select('#reset-button').on('click', resetToLive);
+    d3.select(window).on('resize', resize);
     resize();
+    resetToLive();
 
 })(d3, fc, sc);
