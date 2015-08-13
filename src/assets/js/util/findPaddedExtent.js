f2346202add87fb5a87e4bce08c953ae89210a31
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-13 09:37:44 +0100

    Changed file structure, renamed

--- src/assets/js/util/findPaddedExtent.js
+++ src/assets/js/util/findPaddedExtent.js
@@ -1,6 +1,6 @@
 (function(d3, fc) {
     'use strict';
-    sc.util.padExtent = function(extent, period) {
+    sc.util.findPaddedExtent = function(extent, period) {
         // Adds a buffer of period to either side of data
         extent[0] = d3.time.second.offset(new Date(extent[0]), -period);
         extent[1] = d3.time.second.offset(new Date(extent[1]), +period);
