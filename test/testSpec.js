ec36cd954e7b69281f2ade434cd051c5ed8b4a60
CBarrett2 <chris.barrett2@hotmail.co.uk>
2015-08-26 09:59:06 +0100

    added test test

---
+++ test/testSpec.js
@@ -0,0 +1,10 @@
+(function(sc) {
+    'use strict';
+
+    describe('a test', function() {
+        it('should test something', function() {
+            expect(true).toBeTruthy();
+        });
+    });
+
+})(sc);
